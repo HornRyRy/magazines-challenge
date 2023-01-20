985c6bf7affd0192421fe7e8fb1a5b828bd248f0
Ryan Horn <rj.horn@gmail.com>
2023-01-20 10:29:02 -0700

    solution complete

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,17 @@
 class Magazine < ActiveRecord::Base
-  
+  has_many :subscriptions
+  has_many :readers, through: :subscriptions
+
+
+def email_list
+    self.readers.map{|r| r.email}.join(";")
+    ##self.readers.map(&:email).join(";")  same as line above
+end
+
+def self.most_popular
+    self.all.max_by{|magazine| magazine.subscriptions.length}
+    ##self.preload(:subscriptions).max_by {|magazine| magazine.subscriptions.length}  --> this prevents an n+1 query, or going into the database n+1 times
+end
+
+
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,27 @@
 class Reader < ActiveRecord::Base
-  
+    has_many :subscriptions
+    has_many :magazines, through: :subscriptions
+
+
+    #Visitation.create(date: Date.today, alien_id: self.id, earthling: earthling)
+
+    def subscribe(magazine, price)
+        #Subscription.create(magazine: magazine, price: price, reader: self)
+        self.subscriptions.create(magazine: magazine, price: price)
+    end
+
+    def total_subscription_price
+        self.subscriptions.sum(:price)
+    end
+
+    def cancel_subscription(magazine)
+        ##self.magazines.delete(magazine)  #### doesn't work
+        # ## another solution:
+        # subscription_to_remove = self.subscriptions.where(magazine: magazine).first
+        # Subscription.delete(subscription_to_remove)
+        # ##
+        
+        self.subscriptions.delete(self.subscriptions.where(magazine: magazine))
+    end
+
 end
